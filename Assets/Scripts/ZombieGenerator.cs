8fef335d7a169661fa5a66fc57ae0f6eff2e04e7
Thomas Hartmann <40410024+ThomasHartmannDev@users.noreply.github.com>
2023-04-27 17:49:37 +0200

    .

--- Assets/Scripts/ZombieGenerator.cs
+++ Assets/Scripts/ZombieGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ZombieGenerator : MonoBehaviour
@@ -8,19 +9,29 @@ public class ZombieGenerator : MonoBehaviour
     float timeCounter = 0;
     public float timeGenerator = 1;
     public LayerMask LayerZombie;
+    private float distGen = 3;
 
+    private float distPlayerToGen = 20;
+    private GameObject player;
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
 
     // Update is called once per frame
     void Update()
     {
-        timeCounter += Time.deltaTime;
-        if (timeCounter >= timeGenerator) { 
-            NewZombieGenerator();
-            timeCounter = 0; 
+        if (Vector3.Distance(transform.position, player.transform.position) > distPlayerToGen){
+            timeCounter += Time.deltaTime;
+            if (timeCounter >= timeGenerator)
+            {
+                StartCoroutine(NewZombieGenerator());
+                timeCounter = 0;
+            }
         }
         
     }
-    void NewZombieGenerator()
+    IEnumerator NewZombieGenerator()
     {
         Vector3 posGen = RandomPosition();
         Collider[] col = Physics.OverlapSphere(posGen, 1, LayerZombie);
@@ -28,14 +39,21 @@ public class ZombieGenerator : MonoBehaviour
         {
             posGen = RandomPosition();
             col = Physics.OverlapSphere(posGen, 1, LayerZombie);
+            yield return null;
         }
 
         Instantiate(zombie, posGen, transform.rotation);
     }
 
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, distGen);
+    }
+
     Vector3 RandomPosition()
     {
-        Vector3 pos = Random.insideUnitSphere * 3;
+        Vector3 pos = Random.insideUnitSphere * distGen;
         pos += transform.position;
         pos.y = 0;
         return pos;
